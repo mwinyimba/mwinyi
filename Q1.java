cee83a78319f20eb8450b368700212fa282c9af6
mwinyimba <93635557+mwinyimba@users.noreply.github.com>
2021-11-16 14:33:49 -0500

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,13 @@
+class Q1 {
+    public static void main(String[] args) {
+        System.out.println(" ******         **********      ********     ");
+        System.out.println(" **    **           **          **           ");
+        System.out.println(" **     **          **          **         ");
+        System.out.println(" **      **         **          **          ");
+        System.out.println(" **      **         **          ********     ");
+        System.out.println(" **      **   **    **          **        ");
+        System.out.println(" **     **     **   **          **        ");
+        System.out.println(" **    **       **  **          **        ");
+        System.out.println(" *****            ***           ********     ");
+    }
+}
